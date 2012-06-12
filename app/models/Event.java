1f24e8588be8126e7121aa3c633304cdc06086a0
moyashidaisuke <guitaristdaisuke@gmail.com>
2012-06-13 01:15:39 +0900

    Event削除時にSongも削除されるように修正

--- app/models/Event.java
+++ app/models/Event.java
@@ -14,6 +14,7 @@ import net.sf.oval.guard.Post;
 
 import org.apache.commons.lang.StringUtils;
 import org.h2.engine.User;
+import org.hibernate.annotations.Cascade;
 
 import play.data.validation.MaxSize;
 import play.data.validation.Required;
@@ -40,7 +41,7 @@ public class Event extends Model {
 	@MaxSize(10000)
 	public String content;
 
-	@OneToMany
+	@OneToMany(mappedBy="event",cascade=CascadeType.ALL)
 	public List<Song> song = new ArrayList<Song>();
 
 
